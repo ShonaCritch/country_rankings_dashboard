1f778b4e64c0631010770e25cddb41195e446cd7
Shona Critch <shona.critch@gmail.com>
2021-01-11 01:05:51 +1100

    updated initdb.py

--- initdb.py
+++ initdb.py
@@ -2,3 +2,9 @@ from rankingsdashboard.app import db
 
 # db.drop_all()
 db.create_all()
+
+def create_app(cfg=None):
+    app = Flask(__name__)
+    from api.views import api
+    app.register_blueprint(api)
+    return app
