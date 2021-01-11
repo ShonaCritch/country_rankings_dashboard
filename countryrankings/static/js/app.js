7cd9be99a249fdfa3ca321be1ab61c4865a80a91
sasithwij <sasith.wijebandara@gmail.com>
2021-01-11 21:04:22 +1100

    update to html and javascript

--- countryrankings/static/js/app.js
+++ countryrankings/static/js/app.js
@@ -5,6 +5,52 @@ const ramenurl = "/api/v1.0/ramen"
 const spotifytbody = d3.select("#spotify-table").select("tbody");
 const unitbody = d3.select("#uni-table").select("tbody");
 
+//Build map
+const width = 900;
+const height = 400;
+
+const svg = d3.select('svg').attr('width', width).attr('height', height);
+
+const projection = d3.geoMercator().scale(100)
+    .translate([width / 2, height / 1.4]);
+const path = d3.geoPath(projection);
+
+const g = svg.append('g');
+
+
+d3.json('https://cdn.jsdelivr.net/npm/world-atlas@2/countries-110m.json')
+    .then(data => {
+
+        const countries = topojson.feature(data, data.objects.countries);
+        
+        g.selectAll('path')
+        .data(countries.features)
+        .enter().append('path')
+        .attr('id', function(d) {return d.properties.name.replace(" ", "").replace(" ", "").replace(" ", "")})
+        .attr('d', path)
+      });
+
+//Function to highlight map
+function highlightmap(count) {
+
+  if (count == "USA") {
+    d3.select("#UnitedStatesofAmerica").attr("fill", "#FF0000")
+  }
+  else if (count == "UK") {
+    d3.select("#UnitedKingdom").attr("fill", "#FF0000")
+  }
+  else if (count == "Hong Kong") {
+    d3.select("#HongKong").attr("fill", "#FF0000")
+  }
+  else {
+    d3.select("#" + count).attr("fill", "#FF0000")
+  }
+  console.log("#" + count)
+};
+
+function fillall(count) {
+  d3.selectAll("path").attr("fill", "#000000")
+  console.log(count)}
 
 //Spotify table function//
 function buildSpotifyTable(count) {
@@ -133,38 +179,6 @@ function buildRamenChart(count) {
   });
 };
 
-function buildmap(count) {
-  const width = 900;
-  const height = 400;
-  
-  const svg = d3.select('svg').attr('width', width).attr('height', height);
-  
-  const projection = d3.geoMercator().scale(100)
-      .translate([width / 2, height / 1.4]);
-  const path = d3.geoPath(projection);
-  
-  const g = svg.append('g');
-  
-  d3.json('https://cdn.jsdelivr.net/npm/world-atlas@2/countries-110m.json')
-      .then(data => {
-  
-          const countries = topojson.feature(data, data.objects.countries);
-          
-          g.selectAll('path')
-          .data(countries.features)
-          .enter().append('path')
-          .attr('id', function(d) {return d.properties.name.replace(" ", "").replace(" ", "").replace(" ", "")})
-          .attr('d', path);
-
-          // if (count == "USA") {
-          // d3.select("#UnitedStatesofAmerica").attr("fill", "#FF0000") 
-        // } 
-        // else {
-          d3.select("#" + count).attr("fill", "#FF0000")
-        // }
-        console.log("#" + count)
-      });
-    };
   
 //Get unqiue list of countries    
 function getUnique(arr, comp) {
@@ -206,7 +220,7 @@ function init() {
   buildSpotifyTable(firstCountry);
   buildUniTable(firstCountry);
   buildRamenChart(firstCountry);
-  buildmap(firstCountry)
+  highlightmap(firstCountry)
     });
   });
 };
@@ -214,10 +228,11 @@ function init() {
 
 function optionChanged(newCountry) {
   // Fetch new data each time a new country is selected
+  fillall(newCountry)
   buildSpotifyTable(newCountry);
   buildUniTable(newCountry);
   buildRamenChart(newCountry);
-  buildmap(newCountry);
+  highlightmap(newCountry);
 }
 
 // Initialize the dashboard
